3adc76932738490c67760c4fb31ef26dc5a51b90
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-10-09 06:04:47 +0530

    Gui completed

--- src/Frame1.java
+++ src/Frame1.java
@@ -15,6 +15,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import java.awt.Color;
 import java.awt.SystemColor;
+import java.awt.Font;
 
 
 public class Frame1 {
@@ -48,14 +49,20 @@ public class Frame1 {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-	private void initialize() {
+	private void initialize() {		
 		frame = new JFrame();
 		frame.setBounds(100, 100, 662, 494);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JLabel lblHead= new JLabel("Scalar Pipelined Processor");
+		lblHead.setFont(new Font("Abyssinica SIL", Font.PLAIN, 28));
+		lblHead.setBounds(150, 17, 400, 40);
+		frame.getContentPane().add(lblHead);
+		
 		JEditorPane instructiontext = new JEditorPane();
-		instructiontext.setBounds(40, 32, 196, 120);
+		instructiontext.setFont(new Font("Dialog", Font.BOLD, 15));
+		instructiontext.setBounds(40, 72, 250, 220);
 		frame.getContentPane().add(instructiontext);
 		
 		 btnStart = new JButton("Start");
@@ -64,68 +71,98 @@ public class Frame1 {
 				
 			}
 		});
-		btnStart.setBounds(76, 181, 117, 25);
+		btnStart.setBounds(455, 429, 117, 25);
 		frame.getContentPane().add(btnStart);
 		
 		JLabel lblIf = new JLabel("IF");
-		lblIf.setBounds(301, 30, 30, 15);
+		lblIf.setBounds(301, 70, 30, 15);
 		frame.getContentPane().add(lblIf);
 		
 		JLabel lblId= new JLabel("ID");
-		lblId.setBounds(301, 80, 30, 15);
+		lblId.setBounds(301, 120, 30, 15);
 		frame.getContentPane().add(lblId);
 		
 		JLabel lblRd = new JLabel("RD");
-		lblRd.setBounds(301, 130, 30, 15);
+		lblRd.setBounds(301, 170, 30, 15);
 		frame.getContentPane().add(lblRd);
 		
 		JLabel lblAlu= new JLabel("ALU");
-		lblAlu.setBounds(301, 180, 40, 15);
+		lblAlu.setBounds(301, 220, 40, 15);
 		frame.getContentPane().add(lblAlu);
 		
 		JLabel lblMem = new JLabel("MEM");
-		lblMem.setBounds(301, 230, 40, 15);
+		lblMem.setBounds(301, 270, 40, 15);
 		frame.getContentPane().add(lblMem);
 		
 		JLabel lblWb= new JLabel("WB");
-		lblWb.setBounds(301, 280, 30, 15);
+		lblWb.setBounds(301, 320, 30, 15);
 		frame.getContentPane().add(lblWb);
 		
 		JLabel lblIftxt = new JLabel("");
 		lblIftxt.setOpaque(true);
 		lblIftxt.setBackground(Color.white);
-		lblIftxt.setBounds(365, 25, 200, 25);
+		lblIftxt.setBounds(365, 65, 200, 25);
 		frame.getContentPane().add(lblIftxt);
 		
 		JLabel lblIdtxt = new JLabel("");
 		lblIdtxt.setOpaque(true);
 		lblIdtxt.setBackground(Color.white);
-		lblIdtxt.setBounds(365, 75, 200, 25);
+		lblIdtxt.setBounds(365, 115, 200, 25);
 		frame.getContentPane().add(lblIdtxt);
 		
 		JLabel lblRdtxt = new JLabel("");
 		lblRdtxt.setOpaque(true);
 		lblRdtxt.setBackground(Color.white);
-		lblRdtxt.setBounds(365, 125, 200, 25);
+		lblRdtxt.setBounds(365, 165, 200, 25);
 		frame.getContentPane().add(lblRdtxt);
 		
 		JLabel lblAlutxt = new JLabel("");
 		lblAlutxt.setOpaque(true);
 		lblAlutxt.setBackground(Color.white);
-		lblAlutxt.setBounds(365, 175, 200, 25);
+		lblAlutxt.setBounds(365, 215, 200, 25);
 		frame.getContentPane().add(lblAlutxt);
 		
 		JLabel lblMemtxt = new JLabel("");
 		lblMemtxt.setOpaque(true);
 		lblMemtxt.setBackground(Color.white);
-		lblMemtxt.setBounds(365, 225, 200, 25);
+		lblMemtxt.setBounds(365, 265, 200, 25);
 		frame.getContentPane().add(lblMemtxt);
 		
 		JLabel lblWbtxt = new JLabel("");
 		lblWbtxt.setOpaque(true);
 		lblWbtxt.setBackground(Color.white);
-		lblWbtxt.setBounds(365, 275, 200, 25);
+		lblWbtxt.setBounds(365, 315, 200, 25);
 		frame.getContentPane().add(lblWbtxt);
+		
+		JLabel lblCpi= new JLabel("CPI");
+		lblCpi.setBounds(40, 330, 40, 15);
+		frame.getContentPane().add(lblCpi);
+		
+		JLabel lblCpitxt = new JLabel("");
+		lblCpitxt.setOpaque(true);
+		lblCpitxt.setBackground(Color.white);
+		lblCpitxt.setBounds(170, 325, 120, 25);
+		frame.getContentPane().add(lblCpitxt);
+		
+		JLabel lblStall= new JLabel("NUM OF STALLS");
+		lblStall.setBounds(40, 360, 160, 25);
+		frame.getContentPane().add(lblStall);
+		
+		JLabel lblStalltxt = new JLabel("");
+		lblStalltxt.setOpaque(true);
+		lblStalltxt.setBackground(Color.white);
+		lblStalltxt.setBounds(170, 355, 120, 25);
+		frame.getContentPane().add(lblStalltxt);
+		
+		JLabel lblStallReason= new JLabel("STALL REASON");
+		lblStallReason.setBounds(40, 390, 160, 25);
+		frame.getContentPane().add(lblStallReason);
+		
+		JLabel lblStallReasontxt = new JLabel("");
+		lblStallReasontxt.setOpaque(true);
+		lblStallReasontxt.setBackground(Color.white);
+		lblStallReasontxt.setBounds(170, 385, 120, 25);
+		frame.getContentPane().add(lblStallReasontxt);
 		
 	}
 }
