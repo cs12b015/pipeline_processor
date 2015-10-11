40afd93f8d120ebbc9479a08d2f9a4a8a759545f
cs12b015 <jayanthk@cse.iitm.ac.in>
2015-10-11 18:25:12 +0530

    with r0-r15

--- src/Frame1.java
+++ src/Frame1.java
@@ -54,6 +54,7 @@ public class Frame1 {
 	private Queue<String> sixqueue;
 	private String content = "";
 	private int varpc=0;
+	private JLabel lblR_0txt;
 	private JLabel lblR_1txt;
 	private JLabel lblR_2txt;
 	private JLabel lblR_3txt;
@@ -69,7 +70,7 @@ public class Frame1 {
 	private JLabel lblR_13txt;
 	private JLabel lblR_14txt;
 	private JLabel lblR_15txt;
-	private JLabel lblR_16txt;
+
 
 	private HashMap<Integer,JLabel> regmap=new HashMap<Integer,JLabel>();
 	
@@ -97,7 +98,7 @@ public class Frame1 {
 	public Frame1() throws IOException {
 		
 		InstructionCount=0;
-		BufferedReader br = new BufferedReader(new FileReader("testCases/test3.b"));		
+		BufferedReader br = new BufferedReader(new FileReader("testCases/test4.b"));		
         String line =  null;
 		while((line=br.readLine())!=null){
 		    content=content+"\n"+line;
@@ -307,52 +308,55 @@ public class Frame1 {
 			if(array[0].compareTo("JMP")==0){
 				removeprev();
 			}
-			else if(regflags.get(r2-1)==1)
+			else if(regflags.get(r2)==1)
 			{	
-				regflags.set(r1-1, 1);
-				checkregflags.add(r2-1);
+				regflags.set(r1, 1);
+				checkregflags.add(r2);
 			}
 		}else{			
 			int r1 = Integer.parseInt(bitcode.substring(4,8),2);
 			int r2 = Integer.parseInt(bitcode.substring(8,12),2);
 			int r3 = Integer.parseInt(bitcode.substring(12,16),2);
 			if(array[0].compareTo("LD")==0){
-				regflags.set(r1-1, 1);
-				if(regflags.get(r2-1)==1)
-				{
-					checkregflags.add(r2-1);
+				regflags.set(r1, 1);
+				if(regflags.get(r2)==1)
+				{	if(r1!=r2){
+						checkregflags.add(r2);
+					}
 				}
 			}
 			else if(array[0].compareTo("SD")==0){
-				if(regflags.get(r1-1)==1)
+				if(regflags.get(r1)==1)
 				{
-					checkregflags.add(r1-1);
+					checkregflags.add(r1);
 				}
-				if(regflags.get(r2-1)==1)
+				if(regflags.get(r2)==1)
 				{
-					checkregflags.add(r2-1);
+					checkregflags.add(r2);
 				}
 			}
 			else if(array[0].compareTo("JMP")==0){
 				removeprev();
 			}
 			else if(array[0].compareTo("BEQZ")==0){
-				if(regflags.get(r1-1)==1)
+				if(regflags.get(r1)==1)
 				{
-					checkregflags.add(r1-1);
+					checkregflags.add(r1);
 				}
 			}
 			else if(array[0].compareTo("HLT")==0){
 			}
 			else {
-				regflags.set(r1-1, 1);
-				if(regflags.get(r2-1)==1)
+				regflags.set(r1, 1);
+				if(regflags.get(r2)==1)
 				{
-					checkregflags.add(r2-1);
+					if(r1!=r2)
+					checkregflags.add(r2);
 				}
-				if(regflags.get(r3-1)==1)
+				if(regflags.get(r3)==1)
 				{
-					checkregflags.add(r3-1);
+					if(r1!=r3)
+					checkregflags.add(r3);
 				}
 			}		
 		}	
@@ -390,17 +394,17 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				int temp=registers.get(r2-1)+immedia;
+				int temp=registers.get(r2)+immedia;
 				output ="WB : R"+r1+" <-----"+temp;
-				regflags.set(r1-1, 0);
-				registers.set(r1-1, temp);
+				regflags.set(r1, 0);
+				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				int temp=registers.get(r2-1)+registers.get(r3-1);
+				int temp=registers.get(r2)+registers.get(r3);
 				output ="WB : R"+r1+" <-----"+temp;
-				regflags.set(r1-1, 0);
-				registers.set(r1-1, temp);
+				regflags.set(r1, 0);
+				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}		
 		}else if(array[0].equals("SUB")){
@@ -415,17 +419,17 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				int temp=registers.get(r2-1)-immedia;
+				int temp=registers.get(r2)-immedia;
 				output ="WB : R"+r1+" <-----"+temp;
-				regflags.set(r1-1, 0);
+				regflags.set(r1, 0);
 				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				int temp=registers.get(r2-1)-registers.get(r3-1);
-				regflags.set(r1-1, 0);
+				int temp=registers.get(r2)-registers.get(r3);
+				regflags.set(r1, 0);
 				output ="WB : R"+r1+" <-----"+temp;
-				registers.set(r1-1, temp);
+				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}
 			
@@ -441,17 +445,17 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				int temp=registers.get(r2-1)*immedia;
+				int temp=registers.get(r2)*immedia;
 				output ="WB : R"+r1+" <-----"+temp;
-				regflags.set(r1-1, 0);
-				registers.set(r1-1, temp);
+				regflags.set(r1, 0);
+				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				int temp=registers.get(r2-1)*registers.get(r3-1);
+				int temp=registers.get(r2)*registers.get(r3);
 				output ="WB : R"+r1+" <-----"+temp;
-				regflags.set(r1-1, 0);
-				registers.set(r1-1, temp);
+				regflags.set(r1, 0);
+				registers.set(r1, temp);
 				regmap.get(r1).setText(""+temp);
 			}
 			
@@ -461,7 +465,7 @@ public class Frame1 {
 			output=instruction;
 		}else if(array[0].equals("LD")){
 			int r1 = Integer.parseInt(bitcode.substring(4,8),2);
-			regflags.set(r1-1, 0);
+			regflags.set(r1, 0);
 			output="WB <-- LOAD";
 		}else if(array[0].equals("SD")){
 			output="WB <-- STORE";
@@ -485,15 +489,15 @@ public class Frame1 {
 		if(array[0].equals("LD")){
 			int r1 = Integer.parseInt(bitcode.substring(4,8),2);
 			int r2 = Integer.parseInt(bitcode.substring(8,12),2);
-			registers.set(r1-1, datacache.get(registers.get(r2-1)));
-			regmap.get(r1).setText(""+registers.get(r1-1));
-			output="LOAD R"+r1+" <--- "+datacache.get(registers.get(r2-1));
+			registers.set(r1, datacache.get(registers.get(r2)));
+			regmap.get(r1).setText(""+registers.get(r1));
+			output="LOAD R"+r1+" <--- "+datacache.get(registers.get(r2));
 			
 		}else if(array[0].equals("SD")){
 			int r1 = Integer.parseInt(bitcode.substring(4,8),2);
 			int r2 = Integer.parseInt(bitcode.substring(8,12),2);
-			datacache.set(r1-1, registers.get(r2-1));
-			output="STORE R"+r2+" <--- "+datacache.get(registers.get(r1-1));
+			datacache.set(r1, registers.get(r2));
+			output="STORE R"+r2+" <--- "+datacache.get(registers.get(r1));
 		}else if(array[0].equals("JMP")){
 			JumpFlag=0;
 			int l1 ;
@@ -540,10 +544,10 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				output ="ALUOUT <-- "+registers.get(r2-1)+" + "+immedia;
+				output ="ALUOUT <-- "+registers.get(r2)+" + "+immedia;
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				output ="ALUOUT <-- "+registers.get(r2-1)+" + "+registers.get(r3-1);
+				output ="ALUOUT <-- "+registers.get(r2)+" + "+registers.get(r3);
 			}		
 		}else if(array[0].equals("SUB")){
 			int r1 = Integer.parseInt(bitcode.substring(4,8),2);
@@ -557,10 +561,10 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				output ="ALUOUT <-- "+registers.get(r2-1)+" - "+immedia;
+				output ="ALUOUT <-- "+registers.get(r2)+" - "+immedia;
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				output ="ALUOUT <-- "+registers.get(r2-1)+" - "+registers.get(r3-1);
+				output ="ALUOUT <-- "+registers.get(r2)+" - "+registers.get(r3);
 			}
 			
 		}else if(array[0].equals("MUL")){
@@ -575,10 +579,10 @@ public class Frame1 {
 				else{												
 					immedia = Integer.parseInt(imm,2);
 				}
-				output ="ALUOUT <-- "+registers.get(r2-1)+" * "+immedia;
+				output ="ALUOUT <-- "+registers.get(r2)+" * "+immedia;
 			}else{
 				int r3 = Integer.parseInt(bitcode.substring(12,16),2);
-				output ="ALUOUT <-- "+registers.get(r2-1)+" * "+registers.get(r3-1);
+				output ="ALUOUT <-- "+registers.get(r2)+" * "+registers.get(r3);
 			}
 			
 		}else if(array[0].equals("BEQZ")){
@@ -629,17 +633,17 @@ public class Frame1 {
 		
 		if(instruction.charAt(3)=='1'){
 			int r2 = Integer.parseInt(instruction.substring(8,12),2);
-			output="READ R"+r2+" ==> "+registers.get(r2-1);
+			output="READ R"+r2+" ==> "+registers.get(r2);
 		}
 		else{
 			int r1 = Integer.parseInt(instruction.substring(4,8),2);
 			int r2 = Integer.parseInt(instruction.substring(8,12),2);
 			int r3 = Integer.parseInt(instruction.substring(12,16),2);
 			if(ss.compareTo(s3)==0){
-				output = "READ R"+r2+" ==> "+registers.get(r2-1);
+				output = "READ R"+r2+" ==> "+registers.get(r2);
 			}
 			else if(ss.compareTo(s4)==0){
-				output = "READ R"+r1+" ==> "+registers.get(r1-1);
+				output = "READ R"+r1+" ==> "+registers.get(r1);
 			}
 			else if(ss.compareTo(s5)==0){
 				int l1 ;
@@ -653,13 +657,13 @@ public class Frame1 {
 				output = "JMP" + " " + l1;
 			}
 			else if(ss.compareTo(s6)==0){
-				output = "READ R"+r1+" ==> "+registers.get(r1-1);
+				output = "READ R"+r1+" ==> "+registers.get(r1);
 			}
 			else if(ss.compareTo(s7)==0){
 				output = "HLT" ;
 			}
 			else {
-				output="READ R"+ r2+" ==> "+registers.get(r2-1) +" AND "+"R" + r3+" ==> "+registers.get(r3-1);
+				output="READ R"+ r2+" ==> "+registers.get(r2) +" AND "+"R" + r3+" ==> "+registers.get(r3);
 			}
 	 	}
 		return output;
@@ -843,165 +847,166 @@ public class Frame1 {
 		lblStallReasontxt.setBounds(170, 415, 120, 25);
 		frame.getContentPane().add(lblStallReasontxt);
 		
+		JLabel lblR_0 = new JLabel("R0");
+		lblR_0.setBounds(608, 70, 30, 15);
+		frame.getContentPane().add(lblR_0);
+		
+		lblR_0txt = new JLabel("0");
+		lblR_0txt.setOpaque(true);
+		lblR_0txt.setBackground(Color.cyan);
+		lblR_0txt.setBounds(640, 65, 60, 25);
+		frame.getContentPane().add(lblR_0txt);
+		
 		JLabel lblR_1 = new JLabel("R1");
-		lblR_1.setBounds(608, 70, 30, 15);
+		lblR_1.setBounds(608, 120, 30, 15);
 		frame.getContentPane().add(lblR_1);
 		
 		lblR_1txt = new JLabel("0");
 		lblR_1txt.setOpaque(true);
 		lblR_1txt.setBackground(Color.cyan);
-		lblR_1txt.setBounds(640, 65, 60, 25);
+		lblR_1txt.setBounds(640, 115, 60, 25);
 		frame.getContentPane().add(lblR_1txt);
 		
 		JLabel lblR_2 = new JLabel("R2");
-		lblR_2.setBounds(608, 120, 30, 15);
+		lblR_2.setBounds(608, 170, 30, 15);
 		frame.getContentPane().add(lblR_2);
 		
 		lblR_2txt = new JLabel("0");
 		lblR_2txt.setOpaque(true);
 		lblR_2txt.setBackground(Color.cyan);
-		lblR_2txt.setBounds(640, 115, 60, 25);
+		lblR_2txt.setBounds(640, 165, 60, 25);
 		frame.getContentPane().add(lblR_2txt);
 		
 		JLabel lblR_3 = new JLabel("R3");
-		lblR_3.setBounds(608, 170, 30, 15);
+		lblR_3.setBounds(608, 220, 30, 15);
 		frame.getContentPane().add(lblR_3);
 		
 		lblR_3txt = new JLabel("0");
 		lblR_3txt.setOpaque(true);
 		lblR_3txt.setBackground(Color.cyan);
-		lblR_3txt.setBounds(640, 165, 60, 25);
+		lblR_3txt.setBounds(640, 215, 60, 25);
 		frame.getContentPane().add(lblR_3txt);
 		
 		JLabel lblR_4 = new JLabel("R4");
-		lblR_4.setBounds(608, 220, 30, 15);
+		lblR_4.setBounds(608, 270, 30, 15);
 		frame.getContentPane().add(lblR_4);
 		
 		lblR_4txt = new JLabel("0");
 		lblR_4txt.setOpaque(true);
 		lblR_4txt.setBackground(Color.cyan);
-		lblR_4txt.setBounds(640, 215, 60, 25);
+		lblR_4txt.setBounds(640, 265, 60, 25);
 		frame.getContentPane().add(lblR_4txt);
 		
 		JLabel lblR_5 = new JLabel("R5");
-		lblR_5.setBounds(608, 270, 30, 15);
+		lblR_5.setBounds(608, 320, 30, 15);
 		frame.getContentPane().add(lblR_5);
 		
 		lblR_5txt = new JLabel("0");
 		lblR_5txt.setOpaque(true);
 		lblR_5txt.setBackground(Color.cyan);
-		lblR_5txt.setBounds(640, 265, 60, 25);
+		lblR_5txt.setBounds(640, 315, 60, 25);
 		frame.getContentPane().add(lblR_5txt);
 		
 		JLabel lblR_6 = new JLabel("R6");
-		lblR_6.setBounds(608, 320, 30, 15);
+		lblR_6.setBounds(608, 375, 30, 15);
 		frame.getContentPane().add(lblR_6);
 		
 		lblR_6txt = new JLabel("0");
 		lblR_6txt.setOpaque(true);
 		lblR_6txt.setBackground(Color.cyan);
-		lblR_6txt.setBounds(640, 315, 60, 25);
+		lblR_6txt.setBounds(640, 365, 60, 25);
 		frame.getContentPane().add(lblR_6txt);
 		
 		JLabel lblR_7 = new JLabel("R7");
-		lblR_7.setBounds(608, 375, 30, 15);
+		lblR_7.setBounds(608, 420, 30, 15);
 		frame.getContentPane().add(lblR_7);
 		
 		lblR_7txt = new JLabel("0");
 		lblR_7txt.setOpaque(true);
 		lblR_7txt.setBackground(Color.cyan);
-		lblR_7txt.setBounds(640, 365, 60, 25);
+		lblR_7txt.setBounds(640, 415, 60, 25);
 		frame.getContentPane().add(lblR_7txt);
 		
 		JLabel lblR_8 = new JLabel("R8");
-		lblR_8.setBounds(608, 420, 30, 15);
+		lblR_8.setBounds(720, 70, 30, 15);
 		frame.getContentPane().add(lblR_8);
 		
 		lblR_8txt = new JLabel("0");
 		lblR_8txt.setOpaque(true);
 		lblR_8txt.setBackground(Color.cyan);
-		lblR_8txt.setBounds(640, 415, 60, 25);
+		lblR_8txt.setBounds(750, 65, 60, 25);
 		frame.getContentPane().add(lblR_8txt);
 		
 		JLabel lblR_9 = new JLabel("R9");
-		lblR_9.setBounds(720, 70, 30, 15);
+		lblR_9.setBounds(720, 120, 30, 15);
 		frame.getContentPane().add(lblR_9);
 		
 		lblR_9txt = new JLabel("0");
 		lblR_9txt.setOpaque(true);
 		lblR_9txt.setBackground(Color.cyan);
-		lblR_9txt.setBounds(750, 65, 60, 25);
+		lblR_9txt.setBounds(750, 115, 60, 25);
 		frame.getContentPane().add(lblR_9txt);
 		
 		JLabel lblR_10 = new JLabel("R10");
-		lblR_10.setBounds(720, 120, 30, 15);
+		lblR_10.setBounds(720, 170, 30, 15);
 		frame.getContentPane().add(lblR_10);
 		
 		lblR_10txt = new JLabel("0");
 		lblR_10txt.setOpaque(true);
 		lblR_10txt.setBackground(Color.cyan);
-		lblR_10txt.setBounds(750, 115, 60, 25);
+		lblR_10txt.setBounds(750, 165, 60, 25);
 		frame.getContentPane().add(lblR_10txt);
 		
 		JLabel lblR_11 = new JLabel("R11");
-		lblR_11.setBounds(720, 170, 30, 15);
+		lblR_11.setBounds(720, 220, 30, 15);
 		frame.getContentPane().add(lblR_11);
 		
-		lblR_11txt = new JLabel("0");
-		lblR_11txt.setOpaque(true);
-		lblR_11txt.setBackground(Color.cyan);
-		lblR_11txt.setBounds(750, 165, 60, 25);
-		frame.getContentPane().add(lblR_11txt);
+		lblR_11 = new JLabel("0");
+		lblR_11.setOpaque(true);
+		lblR_11.setBackground(Color.cyan);
+		lblR_11.setBounds(750, 215, 60, 25);
+		frame.getContentPane().add(lblR_11);
 		
 		JLabel lblR_12 = new JLabel("R12");
-		lblR_12.setBounds(720, 220, 30, 15);
+		lblR_12.setBounds(720, 270, 30, 15);
 		frame.getContentPane().add(lblR_12);
 		
-		lblR_12 = new JLabel("0");
-		lblR_12.setOpaque(true);
-		lblR_12.setBackground(Color.cyan);
-		lblR_12.setBounds(750, 215, 60, 25);
-		frame.getContentPane().add(lblR_12);
+		lblR_12txt = new JLabel("0");
+		lblR_12txt.setOpaque(true);
+		lblR_12txt.setBackground(Color.cyan);
+		lblR_12txt.setBounds(750, 265, 60, 25);
+		frame.getContentPane().add(lblR_12txt);
 		
 		JLabel lblR_13 = new JLabel("R13");
-		lblR_13.setBounds(720, 270, 30, 15);
+		lblR_13.setBounds(720, 320, 30, 15);
 		frame.getContentPane().add(lblR_13);
 		
 		lblR_13txt = new JLabel("0");
 		lblR_13txt.setOpaque(true);
 		lblR_13txt.setBackground(Color.cyan);
-		lblR_13txt.setBounds(750, 265, 60, 25);
+		lblR_13txt.setBounds(750, 315, 60, 25);
 		frame.getContentPane().add(lblR_13txt);
 		
 		JLabel lblR_14 = new JLabel("R14");
-		lblR_14.setBounds(720, 320, 30, 15);
+		lblR_14.setBounds(720, 375, 30, 15);
 		frame.getContentPane().add(lblR_14);
 		
 		lblR_14txt = new JLabel("0");
 		lblR_14txt.setOpaque(true);
 		lblR_14txt.setBackground(Color.cyan);
-		lblR_14txt.setBounds(750, 315, 60, 25);
+		lblR_14txt.setBounds(750, 365, 60, 25);
 		frame.getContentPane().add(lblR_14txt);
 		
 		JLabel lblR_15 = new JLabel("R15");
-		lblR_15.setBounds(720, 375, 30, 15);
+		lblR_15.setBounds(720, 420, 30, 15);
 		frame.getContentPane().add(lblR_15);
 		
 		lblR_15txt = new JLabel("0");
 		lblR_15txt.setOpaque(true);
 		lblR_15txt.setBackground(Color.cyan);
-		lblR_15txt.setBounds(750, 365, 60, 25);
+		lblR_15txt.setBounds(750, 415, 60, 25);
 		frame.getContentPane().add(lblR_15txt);
-		
-		JLabel lblR_16 = new JLabel("R16");
-		lblR_16.setBounds(720, 420, 30, 15);
-		frame.getContentPane().add(lblR_16);
-		
-		lblR_16txt = new JLabel("0");
-		lblR_16txt.setOpaque(true);
-		lblR_16txt.setBackground(Color.cyan);
-		lblR_16txt.setBounds(750, 415, 60, 25);
-		frame.getContentPane().add(lblR_16txt);
+		regmap.put(0, lblR_0txt);
 		regmap.put(1, lblR_1txt);
 		regmap.put(2, lblR_2txt);
 		regmap.put(3, lblR_3txt);
@@ -1017,7 +1022,6 @@ public class Frame1 {
 		regmap.put(13, lblR_13txt);
 		regmap.put(14, lblR_14txt);
 		regmap.put(15, lblR_15txt);
-		regmap.put(16, lblR_16txt);
 		
 		
 	}
